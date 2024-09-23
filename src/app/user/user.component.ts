22d8e69a3ba655d6b18f427e2e95e6b1c34b8fde
Lincoln Araujo <lincoln.araujo@ebc.com.br>
2024-09-23 09:51:31 -0300

    adding task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+//type User = {
+//  id: string;
+//  avatar: string;
+//  name: string;
+//}
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,16 +20,14 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
