f4b58575e8b4717da541108e3a8ec4256ed42fc3
Lincoln Araujo <lincoln.araujo@ebc.com.br>
2024-09-27 09:43:01 -0300

    setting module components

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,19 +1,16 @@
 import { Component, EventEmitter, Input, Output, inject  } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { type NewTaskData } from './new-task.model';
+
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Input({required: true}) userId!: string;
   @Output() close  = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>()
+
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
